ebe61dc17c9fee7d5b70e2d8c872ac9eaf7e3f5c
1190202328 <1259029328@qq.com>
2022-10-09 19:07:12 +0800

    跑成功！等待结果

--- configs/recognition/tsn/my_tsn_r50_video_1x1x8_100e_kinetics400_rgb.py
+++ configs/recognition/tsn/my_tsn_r50_video_1x1x8_100e_kinetics400_rgb.py
@@ -62,7 +62,7 @@ test_pipeline = [
     dict(type='ToTensor', keys=['imgs'])
 ]
 data = dict(
-    videos_per_gpu=32,
+    videos_per_gpu=8,
     workers_per_gpu=8,
     test_dataloader=dict(videos_per_gpu=1),
     train=dict(
